2f9aea091a0fc6a6ac8eccebbedba0b77c39894f
RohanTrix <rohanyeahyeah@gmail.com>
2020-09-22 01:18:36 +0530

    cin and cout will be faster with addition of these 2 lines

--- Leetcode/A.cpp
+++ Leetcode/A.cpp
@@ -3,6 +3,8 @@
 using namespace std;
 int main()
 {
+    ios_base::sync_with_stdio(0);
+    cin.tie(0); cout.tie(0);
     freopen("input.txt", "r", stdin);
     freopen("output.txt", "w", stdout);
     int a;
