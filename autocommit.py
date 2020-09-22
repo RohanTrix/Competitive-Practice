201d21ca56a7b3ac75eceeaac0acff2e9e4ce260
RohanTrix <rohanyeahyeah@gmail.com>
2020-09-22 16:10:53 +0530

    Committed changes

--- autocommit.py
+++ autocommit.py
@@ -3,6 +3,8 @@ import time
 while(True):
     if os.system('git diff --exit-code')==0:
         print(0)
+        time.sleep(60)
+        os.system('git add .')
         continue
     os.system('git add-commit -m "Committed changes"')
     os.system('git push -u origin master')
