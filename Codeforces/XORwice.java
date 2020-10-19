2ed89d52e436d13a41069f703a34c72eaf22351a
RohanTrix <rohanyeahyeah@gmail.com>
2020-10-19 10:29:15 +0530

    Committed changes

---
+++ Codeforces/XORwice.java
@@ -0,0 +1,130 @@
+import java.io.BufferedReader; 
+import java.io.IOException; 
+import java.io.InputStreamReader;  
+import java.util.StringTokenizer;
+import java.io.OutputStreamWriter;
+import java.io.PrintWriter;
+import java.io.FileReader;
+public class XORwice
+{
+    public static void main(String[] args) 
+    {
+        // FastReader(true)         for File I/O
+        // FastReader()             for terminal I/O    
+
+        FastReader sc=new FastReader(true); 
+
+        //CODE BEGIN
+        int t = sc.nextInt();
+        for(int i=0;i<t;i++)
+        { 
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+        int x = a & b;
+        sc.println((a^x) + (b^x));
+        }
+        //CODE END
+        // sc.flush() if the output needs to be flushed
+        sc.closer();
+        
+    }
+    static class FastReader 
+    { 
+        BufferedReader br; 
+        StringTokenizer st;
+        PrintWriter pw;
+  
+        public FastReader() 
+        { 
+            br = new BufferedReader(new
+                     InputStreamReader(System.in));
+            pw = new PrintWriter(new OutputStreamWriter(System.out));
+        }
+        public FastReader(boolean b)
+        {
+            try
+            {
+                br = new BufferedReader( new FileReader("input.txt")); 
+                pw = new PrintWriter("output.txt");
+            
+            }
+            catch(Exception e)
+            {
+
+            }
+
+        }
+        String next() 
+        { 
+            while (st == null || !st.hasMoreElements()) 
+            { 
+                try
+                { 
+                    st = new StringTokenizer(br.readLine()); 
+                } 
+                catch (IOException  e) 
+                { 
+                    e.printStackTrace(); 
+                } 
+            } 
+            return st.nextToken();
+        } 
+  
+        int nextInt() 
+        { 
+            return Integer.parseInt(next()); 
+        } 
+  
+        long nextLong() 
+        { 
+            return Long.parseLong(next()); 
+        } 
+  
+        double nextDouble() 
+        { 
+            return Double.parseDouble(next()); 
+        } 
+  
+        String nextLine() 
+        { 
+            String str = ""; 
+            try
+            { 
+                str = br.readLine(); 
+            } 
+            catch (IOException e) 
+            { 
+                e.printStackTrace(); 
+            } 
+            return str; 
+        }
+        void print(Object...objects) {
+            for (int i = 0; i < objects.length; i++) {
+                if (i != 0)
+                    pw.print(' ');
+            pw.print(objects[i]);
+            }
+        }
+        void println(Object...objects)
+        {
+            for (int i = 0; i < objects.length; i++) {
+                if (i != 0)
+                    pw.print(' ');
+            pw.print(objects[i]);
+            }
+            pw.println();
+        }
+        void closer()
+        {
+            try{
+            br.close();
+            pw.flush();
+            pw.close();
+            }
+            catch(Exception e)
+            {
+            }
+        
+        }
+    } 
+}
