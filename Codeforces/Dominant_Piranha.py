ade1ddbff2993c12abfd75dd6e4431038a42ed4b
RohanTrix <rohanyeahyeah@gmail.com>
2020-10-20 23:06:19 +0530

    Committed changes

--- Codeforces/Dominant_Piranha.py
+++ Codeforces/Dominant_Piranha.py
@@ -9,4 +9,8 @@ for t in range(int(input())):
             m= l[i]
     for i in range(n):
         if l[i]==m:
+            al.append(i)
+    for i in al:
+        if i==0 and l[i+1] <m:
+            print(i+1)
             
