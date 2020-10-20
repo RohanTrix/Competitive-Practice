2fb120e181358b4646e848f718bdd15245a216c2
RohanTrix <rohanyeahyeah@gmail.com>
2020-10-20 23:05:11 +0530

    Committed changes

--- Codeforces/Dominant_Piranha.py
+++ Codeforces/Dominant_Piranha.py
@@ -2,5 +2,11 @@ for t in range(int(input())):
     n = int(input())
     l = list(map(int,input().split()))
     al = []
+    m=-99999999
+    b = False
     for i in range(n):
-        m
+        if m < l[i]:
+            m= l[i]
+    for i in range(n):
+        if l[i]==m:
+            
