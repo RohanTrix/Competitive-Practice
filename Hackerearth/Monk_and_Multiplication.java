5410a220ef1bd26167daaa6e96c0b9035ad70f04
RohanTrix <rohanyeahyeah@gmail.com>
2020-10-27 23:35:19 +0530

    Changes to Template

--- Hackerearth/Monk_and_Multiplication.java
+++ Hackerearth/Monk_and_Multiplication.java
@@ -1,9 +1,19 @@
-import java.io.*;
-import java.util.*;
+import java.io.BufferedReader; 
+import java.io.IOException; 
+import java.io.InputStreamReader;  
+import java.util.StringTokenizer;
+import java.io.OutputStreamWriter;
+import java.io.PrintWriter;
+import java.io.FileReader;
 public class Template 
 {
     static void solve(FastReader sc)
     {
+        int n  =sc.nextInt();
+        int arr[] = new int[n];
+        for(int i=0;i<n;i++)
+        arr[i]= sc.nextInt();
+        
         
 
         
