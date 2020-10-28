b848091cee90033b64f329d67860e68d3a588d2e
RohanTrix <rohanyeahyeah@gmail.com>
2020-10-29 01:33:34 +0530

    Committed changes

--- Codechef/javarun.py
+++ Codechef/javarun.py
@@ -3,6 +3,6 @@ import time
 prog_name = "*.java"
 os.system(f'javac -d Java-Classes {prog_name}')
 t1 = time.time()
-os.system(f'java -cp Java-Classes ASP')
+os.system(f'java -cp Java-Classes Monk_And_Champions_League local')
 t2 = time.time()
 print("TIME TAKEN: ",t2-t1)
