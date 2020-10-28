9905c9385aae4a6853c829315e55c664c909c479
RohanTrix <rohanyeahyeah@gmail.com>
2020-10-29 01:34:43 +0530

    Committed changes

--- SPOJ/javarun.py
+++ SPOJ/javarun.py
@@ -1,8 +1,8 @@
 import os
 import time
 prog_name = "*.java"
-os.system(f'javac -d Classes {prog_name}')
+os.system(f'javac -d Java-Classes {prog_name}')
 t1 = time.time()
-os.system(f'java -cp Classes My_Fast_IO')
+os.system(f'java -cp Java-Classes Monk_And_Champions_League local')
 t2 = time.time()
 print("TIME TAKEN: ",t2-t1)
