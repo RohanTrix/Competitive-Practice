17162d3ef901dbda6b84e16d4dd5e9ec26229f6c
RohanTrix <rohanyeahyeah@gmail.com>
2020-11-02 18:09:04 +0530

    Template Updated-- Sorting for arrays using static method Sort

--- Template.java
+++ Template.java
@@ -29,6 +29,12 @@ public class Template
 
         }
     }
+    static void sort(int[] a) {
+		ArrayList<Integer> l=new ArrayList<>();
+		for (int i:a) l.add(i);
+		Collections.sort(l);
+		for (int i=0; i<a.length; i++) a[i]=l.get(i);
+	}
     static class pair implements Comparable < pair >
     {
         long x;
