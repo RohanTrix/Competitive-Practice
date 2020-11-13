ab6987b51a80d1c49ff1372c9ecf9ec22e355e44
RohanTrix <rohanyeahyeah@gmail.com>
2020-11-14 02:16:21 +0530

    Committed changes

---
+++ EXTRA/Coursera-Algorithms-On-Graphs/week3_paths_in_graphs1/1_flight_segments/BFS.java
@@ -0,0 +1,175 @@
+import java.util.*;
+import  java.io.*;
+public class BFS {
+  // Adjaceny List
+  public Map<Integer, Set<Integer>> edges = new TreeMap<>();
+  // Visited Set
+  public static Set<Integer> visited = new HashSet<Integer>();
+  public static void main(String[] args)
+  {
+    FastReader sc=new FastReader();
+    //System.out.println("Enter the number of nodes:");
+    int n = sc.nextInt();
+    //System.out.println("Enter the number of edges:");
+    int m = sc.nextInt();
+    BFS g = new BFS();
+    for(int i =1; i<=n;i++)
+    {
+      g.addNode(i);
+    }
+    for(int i =0; i< n;i++)
+    {
+      g.addEdge(sc.nextInt(), sc.nextInt());
+    }
+    int start = sc.nextInt();
+    g.bfs(start));
+    sc.close();
+  } 
+  void bfs(int v)
+  {
+    int end = sc.nextInt();
+    Queue<Integer> q = new LinkedList<>();
+    int distance[] = new int[edges.size()+1];
+    Arrays.fill(distance, -1);
+    visited.add(v);
+    distance[v] = 0;
+    q.add(v);
+    while(q.size()!=0)
+    {
+      int s = q.poll();
+      // Process node s
+      for( int u : edges.get(s))
+      {
+        if(visited.contains(u))
+          continue;
+        visited.add(u);
+        distance[u] = distance[s] + 1;
+        q.add(u);
+      }
+    }
+
+    //System.out.println( Arrays.toString(distance));
+  }
+  static class FastReader 
+    { 
+        BufferedReader br; 
+        StringTokenizer st;
+        PrintWriter pw;
+  
+        public FastReader() 
+        { 
+            br = new BufferedReader(new
+                     InputStreamReader(System.in));
+            pw = new PrintWriter(new OutputStreamWriter(System.out));
+        }
+        public FastReader(boolean b)
+        {
+            try
+            {
+                br = new BufferedReader( new FileReader("input.txt")); 
+                pw = new PrintWriter("output.txt");
+            
+            }
+            catch(Exception e)
+            {
+
+            }
+
+        }
+        String next() 
+        { 
+            while (st == null || !st.hasMoreElements()) 
+            { 
+                try
+                { 
+                    st = new StringTokenizer(br.readLine()); 
+                } 
+                catch (IOException  e) 
+                { 
+                    e.printStackTrace(); 
+                } 
+            } 
+            return st.nextToken();
+        } 
+  
+        int nextInt() 
+        { 
+            return Integer.parseInt(next()); 
+        } 
+  
+        long nextLong() 
+        { 
+            return Long.parseLong(next()); 
+        } 
+  
+        double nextDouble() 
+        { 
+            return Double.parseDouble(next()); 
+        } 
+  
+        String nextLine() 
+        { 
+            String str = ""; 
+            try
+            { 
+                str = br.readLine(); 
+            } 
+            catch (IOException e) 
+            { 
+                e.printStackTrace(); 
+            } 
+            return str; 
+        }
+        void print(Object...objects) {
+            for (int i = 0; i < objects.length; i++) {
+                if (i != 0)
+                    pw.print(' ');
+            pw.print(objects[i]);
+            }
+        }
+        void println(Object...objects)
+        {
+            for (int i = 0; i < objects.length; i++) {
+                if (i != 0)
+                    pw.print(' ');
+            pw.print(objects[i]);
+            }
+            pw.println();
+        }
+        void closer()
+        {
+            try{
+            br.close();
+            pw.flush();
+            pw.close();
+            }
+            catch(Exception e)
+            {
+            }
+        
+        }
+    }
+  public void addNode(int u) {
+    if (!edges.containsKey(u)) {
+      edges.put(u, new TreeSet<Integer>());
+    }
+  }
+  public void removeNode(int u) {
+    if (!edges.containsKey(u)) {
+      return;
+    }
+    for (int v : edges.get(u)) {
+      edges.get(v).remove(u);
+    }
+    edges.remove(u);
+  }
+  public void addEdge(int u, int v) {
+    edges.get(u).add(v);
+    edges.get(v).add(u);
+
+  }
+
+  public void removeEdge(int u, int v) {
+    edges.get(u).remove(v);
+  }
+}
