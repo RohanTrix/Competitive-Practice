712a4758b99ba90010268b8877ad2a180b867223
RohanTrix <rohanyeahyeah@gmail.com>
2020-11-13 12:01:41 +0530

    Solved Strongly connected Components using Kosaraju's Algorithm in Coursera course

--- EXTRA/Coursera-Algorithms-On-Graphs/week2_graph_decomposition2/3_intersection_reachability/StronglyConnected.java
+++ EXTRA/Coursera-Algorithms-On-Graphs/week2_graph_decomposition2/3_intersection_reachability/StronglyConnected.java
@@ -1,28 +1,193 @@
-import java.util.ArrayList;
-import java.util.Collections;
-import java.util.Scanner;
-
+import java.util.*;
+import java.io.*;
 public class StronglyConnected {
-    private static int numberOfStronglyConnectedComponents(ArrayList<Integer>[] adj) {
-        //write your code here
-        return 0;
+  // Adjaceny List
+  public Map<Integer, Set<Integer>> edges = new TreeMap<>();
+  // Visited Set
+  public static Set<Integer> visited = new HashSet<Integer>();
+  public static List<Integer> stack = new ArrayList<>();
+  public static void main(String[] args)
+  {
+    FastReader sc=new FastReader();
+    //System.out.println("Enter the number of nodes:");
+    int n = sc.nextInt();
+    //System.out.println("Enter the number of edges:");
+    int m = sc.nextInt();
+    StronglyConnected g = new StronglyConnected();
+    StronglyConnected rev_g = new StronglyConnected();
+    for(int i =1; i<=n;i++)
+    {
+      g.addNode(i);
+      rev_g.addNode(i);
     }
+    for(int i =0; i< m;i++)
+    {
+        int a = sc.nextInt(); int b = sc.nextInt();
+      g.addEdge(a, b);
+      rev_g.addEdge(b, a);
+    }
+    // FIRST-PASS
+    for(int i =1; i<=n;i++)
+    {
+        if(!visited.contains(i))
+            g.dfs(i);
+    }
+    // SECOND-PASS....now operate on reverse graph
+    visited.clear();
+    int i=0,cnt=0;
+    while(stack.size()!=0)
+    {
+        i = stack.remove(stack.size()-1);
+        if(!visited.contains(i)){
+            rev_g.dfs2(i);
+            cnt+=1;
+        }
+    }
+    sc.print(cnt);
+    sc.closer();
+  }
+  void dfs(int v)
+  {
+    if(visited.contains(v))
+        return;
+    visited.add(v);
+    for(int u : edges.get(v))
+        {
+            dfs(u);
+        }
+    stack.add(v);
+  }
+  void dfs2(int v)
+  {
+    if(visited.contains(v))
+    {
+        stack.remove(stack.size()-1);
+        return;
+    }
+    visited.add(v);
+    for(int u : edges.get(v))
+        {
+            dfs(u);
+        }
+  }
+  static class FastReader 
+    { 
+        BufferedReader br; 
+        StringTokenizer st;
+        PrintWriter pw;
+  
+        public FastReader() 
+        { 
+            br = new BufferedReader(new
+                     InputStreamReader(System.in));
+            pw = new PrintWriter(new OutputStreamWriter(System.out));
+        }
+        public FastReader(boolean b)
+        {
+            try
+            {
+                br = new BufferedReader( new FileReader("input.txt")); 
+                pw = new PrintWriter("output.txt");
+            
+            }
+            catch(Exception e)
+            {
 
-    public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
-        int n = scanner.nextInt();
-        int m = scanner.nextInt();
-        ArrayList<Integer>[] adj = (ArrayList<Integer>[])new ArrayList[n];
-        for (int i = 0; i < n; i++) {
-            adj[i] = new ArrayList<Integer>();
+            }
+
         }
-        for (int i = 0; i < m; i++) {
-            int x, y;
-            x = scanner.nextInt();
-            y = scanner.nextInt();
-            adj[x - 1].add(y - 1);
+        String next() 
+        { 
+            while (st == null || !st.hasMoreElements()) 
+            { 
+                try
+                { 
+                    st = new StringTokenizer(br.readLine()); 
+                } 
+                catch (IOException  e) 
+                { 
+                    e.printStackTrace(); 
+                } 
+            } 
+            return st.nextToken();
+        } 
+  
+        int nextInt() 
+        { 
+            return Integer.parseInt(next()); 
+        } 
+  
+        long nextLong() 
+        { 
+            return Long.parseLong(next()); 
+        } 
+  
+        double nextDouble() 
+        { 
+            return Double.parseDouble(next()); 
+        } 
+  
+        String nextLine() 
+        { 
+            String str = ""; 
+            try
+            { 
+                str = br.readLine(); 
+            } 
+            catch (IOException e) 
+            { 
+                e.printStackTrace(); 
+            } 
+            return str; 
         }
-        System.out.println(numberOfStronglyConnectedComponents(adj));
+        void print(Object...objects) {
+            for (int i = 0; i < objects.length; i++) {
+                if (i != 0)
+                    pw.print(' ');
+            pw.print(objects[i]);
+            }
+        }
+        void println(Object...objects)
+        {
+            for (int i = 0; i < objects.length; i++) {
+                if (i != 0)
+                    pw.print(' ');
+            pw.print(objects[i]);
+            }
+            pw.println();
+        }
+        void closer()
+        {
+            try{
+            br.close();
+            pw.flush();
+            pw.close();
+            }
+            catch(Exception e)
+            {
+            }
+        
+        }
     }
+  public void addNode(int u) {
+    if (!edges.containsKey(u)) {
+      edges.put(u, new TreeSet<Integer>());
+    }
+  }
+  public void removeNode(int u) {
+    if (!edges.containsKey(u)) {
+      return;
+    }
+    for (int v : edges.get(u)) {
+      edges.get(v).remove(u);
+    }
+    edges.remove(u);
+  }
+  public void addEdge(int u, int v) {
+    edges.get(u).add(v);
+  }
+
+  public void removeEdge(int u, int v) {
+    edges.get(u).remove(v);
+  }
 }
-
