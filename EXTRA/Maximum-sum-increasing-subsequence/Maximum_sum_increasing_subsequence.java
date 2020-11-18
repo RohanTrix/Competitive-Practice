3655b021847ca90b2c5c2397b5b8aa660c0f4925
RohanTrix <rohanyeahyeah@gmail.com>
2020-11-18 11:06:43 +0530

    Solved Maximum Sum Increasing Subsequence

---
+++ EXTRA/Maximum-sum-increasing-subsequence/Maximum_sum_increasing_subsequence.java
@@ -0,0 +1,47 @@
+// { Driver Code Starts
+//Initial Template for Java
+
+import java.io.*;
+import java.util.*;
+public class Maximum_sum_increasing_subsequence
+{
+    public static void main(String args[])
+        {
+            Scanner sc = new Scanner(System.in);
+            int t = sc.nextInt();
+            while(t-->0)
+                {
+                    int n = sc.nextInt();
+                    int Arr[] = new int[n];
+                    for(int i = 0;i<n;i++)
+                        Arr[i] = sc.nextInt();
+                    Solution ob = new Solution();
+                    System.out.println(ob.maxSumIS(Arr,n));
+                }
+        }
+}  
+
+class Solution
+{
+	public int maxSumIS(int arr[], int n)  
+	{  
+	    int i,j;
+	    int trackSum[] = new int[n];
+	    for(i=0; i< n;i++)
+	    trackSum[i] = arr[i];
+	    for(i = 0;i < n; i++)
+	    {
+	        int currNum = arr[i];
+	        
+	        for(j = 0; j<i; j++)
+	        {
+	            if( currNum > arr[j])
+	                trackSum[i] = Math.max(trackSum[i],trackSum[j] + arr[i]);
+	        }
+	    }
+	    int maxi = Integer.MIN_VALUE;
+	    for(int el : trackSum)
+	        maxi = Math.max(maxi, el);
+	    return maxi;
+	}  
+}
---
+++ EXTRA/Maximum-sum-increasing-subsequence/README.md
@@ -0,0 +1,2 @@
+# Question:
+Given an array arr of N positive integers, the task is to find the maximum sum increasing subsequence of the given array.
