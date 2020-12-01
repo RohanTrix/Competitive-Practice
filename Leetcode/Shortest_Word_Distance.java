0a3cc7ca51706a6283a60ac612013e06606e5c7d
RohanTrix <rohanyeahyeah@gmail.com>
2020-12-01 15:18:45 +0530

    Solved Shortest Word distance Problem on Leetcode

--- Leetcode/Shortest_Word_Distance.java
+++ Leetcode/Shortest_Word_Distance.java
@@ -1,3 +1,18 @@
 public class Shortest_Word_Distance {
+    public int shortestDistance(String[] words, String word1, String word2) {
+        int i1 = -1, i2 = -1;
+        int minDistance = words.length;
+        for (int i = 0; i < words.length; i++) {
+            if (words[i].equals(word1)) {
+                i1 = i;
+            } else if (words[i].equals(word2)) {
+                i2 = i;
+            }
     
+            if (i1 != -1 && i2 != -1) {
+                minDistance = Math.min(minDistance, Math.abs(i1 - i2));
+            }
+        }
+        return minDistance;
+    }
 }
