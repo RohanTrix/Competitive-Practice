0c2bb5efae6da258cc2c41112856cb14fdb60c71
RohanTrix <rohanyeahyeah@gmail.com>
2020-12-01 15:20:07 +0530

    Added Algo for Shortest Word Distance problem of Leetcode Premium

--- Leetcode/Shortest_Word_Distance.java
+++ Leetcode/Shortest_Word_Distance.java
@@ -1,7 +1,12 @@
 public class Shortest_Word_Distance {
 
+    /* We can greatly improve on the brute-force approach by keeping two indices i1 and i2
+        where we store the most recent locations of word1 and word2.
+        Each time we find a new occurrence of one of the words, we do not 
+        need to search the entire array for the other word,
+        since we already have the index of its most recent occurrence.
+     */
 
-    
     public int shortestDistance(String[] words, String word1, String word2) {
         int i1 = -1, i2 = -1;
         int minDistance = words.length;
