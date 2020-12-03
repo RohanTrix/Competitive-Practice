5170126ad33c51811837c6c00f883fe641ca520c
RohanTrix <rohanyeahyeah@gmail.com>
2020-12-03 18:44:23 +0530

    Committed changes

--- Codechef/javarun.py
+++ Codechef/javarun.py
@@ -3,6 +3,6 @@ import time
 prog_name = "*.java"
 os.system(f'javac -d Java-Classes {prog_name}')
 t1 = time.time()
-os.system(f'java -cp Java-Classes ECPC10A local')
+os.system(f'java -cp Java-Classes ANITGUY2 local')
 t2 = time.time()
 print("TIME TAKEN: ",t2-t1)
