b02a0968bff99f7f04dfc0411774278f80f590d8
RohanTrix <rohanyeahyeah@gmail.com>
2020-12-06 14:55:52 +0530

    Changes

--- Contest/HAIL.java
+++ Contest/HAIL.java
@@ -1,9 +1,72 @@
 import java.io.*;
 import java.util.*;
-public class B 
+public class HAIL
 {
     static void solve(FastReader sc)
     {
+        int n = sc.nextInt(); int x = sc.nextInt();
+        int bitset[][] = new int[30+1][n+1];
+        int xor =0;int k, totPair = 0;
+        int arr[] = new int[n+1];
+        for(int j=1;j<=n;j++)
+        {
+            arr[j] = sc.nextInt();
+            xor^=arr[j];
+            k=1;
+            for(int i=30;i>=1;i--)
+                {
+                    //sc.println("k="+k+"  "+((arr[j] & (1 << (k - 1))) >> (k - 1)));
+                    bitset[i][j] = ((arr[j] & (1 << (k - 1))) >> (k - 1));
+                    k++;
+                }
+        }
+        //sc.viewArray1D(arr);
+        //sc.viewArray2D(bitset);
+        HashMap<Integer, Queue<Integer>> map = new HashMap<>();
+        int sum = 0;
+        for(int i=1;i<=30;i++)
+        {
+            sum=0;
+            map.put(i, new LinkedList<Integer>());
+            for(int j =1; j<=n;j++)
+            {
+                if( bitset[i][j] == 1)
+                {
+                    sum++;
+                    map.get(i).offer(j);
+                }
+            }
+            totPair+= (sum/2) +(sum%2);
+            if( map.get(i).size()%2!=0)
+                map.get(i).offer(n);
+        }
+        if(totPair >=x)
+        {
+            for(int i=1;i<=n-1;i++)
+            {
+                arr[i] = 0;
+            }
+            arr[n]=xor;
+        }
+        else
+        {
+            for(int j =1;j<=n;j++)
+            {
+                for(int i=1;i<=30;i++)
+                {
+                    if(x>0 && map.get(i).size()!=0 && j==map.get(i).peek())
+                    {
+                        int a = map.get(i).remove();
+                        int b = map.get(i).remove();
+                        arr[a] ^= (1 << ((30-i+1)-1));
+                        arr[b] ^= (1 << ((30-i+1)-1));
+                        x--;
+                    }
+                }
+            }
+        }
+        for(int i=1;i<=n;i++)
+        sc.print(arr[i]+" ");
 
 
         
