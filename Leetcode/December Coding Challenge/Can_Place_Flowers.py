1b86aa856dc20dd703f9a9dd3393007603fd8d6a
RohanTrix <rohanyeahyeah@gmail.com>
2020-12-06 11:53:48 +0530

    Solved Dec Leetcode ques Day 5

---
+++ Leetcode/December Coding Challenge/Can_Place_Flowers.py
@@ -0,0 +1,10 @@
+class Can_Place_Flowers:
+    def canPlaceFlowers(self, flowerbed: List[int], n: int) -> bool:
+        count = 0
+        i = 0
+        while i < len(flowerbed):
+            if flowerbed[i] == 0 and (i == 0 or flowerbed[i - 1] == 0) and (i == len(flowerbed) - 1 or flowerbed[i + 1] == 0):
+                flowerbed[i] = 1
+                count = count + 1
+            i+=1
+        return count >= n
