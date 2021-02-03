f57667f91896567c50be9f697fdb335c0b28e920
RohanTrix <rohanyeahyeah@gmail.com>
2021-02-03 10:57:39 +0530

    Changes

--- Contest/javarun.py
+++ Contest/javarun.py
@@ -1,17 +1,8 @@
 import os
 import time
-import tkinter as tk
-from tkinter import ttk
-win = tk.Tk()  
-win.title("JAVARUN")
-def click_me():
-    prog_name = "B.java"
-    os.system(f'javac -d Classes {prog_name}')
-    t1 = time.time()
-    os.system(f'java -cp Classes B local')
-    t2 = time.time()
-    print("TIME TAKEN: ",t2-t1)
-# Adding a button
-action = ttk.Button(win, text = "Click me!", command = click_me)
-action.grid(column=1, row=0)
-win.mainloop()
+prog_name = "Tree.java"
+os.system(f'javac -d Classes {prog_name}')
+t1 = time.time()
+os.system(f'java -cp Classes Tree local')
+t2 = time.time()
+print("TIME TAKEN: ",t2-t1)
