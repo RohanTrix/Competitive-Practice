b9d2e69ad1f1e61d58b233fe3e8b35d7a695fccf
RohanTrix <52482556+RohanTrix@users.noreply.github.com>
2021-02-17 12:59:07 +0000

    Changes

--- Atcoder DP/F.java
+++ Atcoder DP/F.java
@@ -1,221 +1,32 @@
-import java.io.*;
 import java.util.*;
+
 public class F
 {
-    //Refer AlgoExpert
-    static void solve(FastReader sc)
-    {
-       
-        String first = sc.next();
-		String second = sc.next();
-
-		int[][] dp = new int[first.length()+1][second.length()+1];
-		for(int i = 1; i <= first.length(); i ++) 
-        {
-			for(int j = 1; j <= second.length(); j ++)
-             {
-				if(first.charAt(i-1) == second.charAt(j-1))
-                {
-					dp[i][j] = dp[i-1][j-1] + 1;
+	public static void main(String[] args) {
+		Scanner in = new Scanner(System.in);
+		String s = in.next(), t = in.next();
+        //Finding length of LCS using DP
+		int[][] dp = new int[s.length() + 1][t.length() + 1];
+		for(int i=s.length() - 1; i>=0; i--) {
+			for(int j=t.length() - 1; j>=0; j--) {
+				if(s.charAt(i) == t.charAt(j)) {
+					dp[i][j] = dp[i+1][j+1] + 1;
+					continue;
 				}
-				else {
-					dp[i][j] = Math.max(dp[i-1][j], dp[i][j-1]);
-				}
+				dp[i][j] = Math.max(dp[i][j+1], dp[i+1][j]);
 			}
 		}
-
-
-           
-    
-    
-    public static void main(String[] args) 
-    {
-        // FastReader(true)         for File I/O
-        // FastReader()             for terminal I/O
-        if(args.length>0 && args[0].equals("local")){
-            FastReader sc=new FastReader(true); 
-            //CODE BEGIN
-            //for(int T = sc.nextInt();T > 0;T--)
-            solve(sc);
-            //CODE END
-            sc.closer();
-        }
-        else
-        {
-            FastReader sc=new FastReader(); 
-            //CODE BEGIN
-            //for(int T = sc.nextInt();T > 0;T--)
-            solve(sc);
-            //CODE END
-            sc.closer();
-
-        }
-    }
-    static int gcd(int a, int b)
-	{
-		if(b == 0)
-		 return a;
-		else
-		return gcd(b,a%b);
-    }
-    public static int lcm(int a, int b)
-    {
-        return (a / gcd(a, b)) * b;
-    }
-    static void sort(int[] a) {
-		ArrayList<Integer> l=new ArrayList<>();
-		for (int i:a) l.add(i);
-		Collections.sort(l);
-		for (int i=0; i<a.length; i++) a[i]=l.get(i);
-    }
-    static void fill2D(int arr[][], int n)
-    {
-        for (int[] row: arr)
-            Arrays.fill(row, n);
-    }
-    static class pair implements Comparable < pair >
-    {
-        long x;
-        long y;
-        pair(long i, long j) {
-            x = i;
-            y = j;
-        }
-        public int compareTo(pair p) {
-            if (this.x != p.x) {
-                return Long.compare(this.x,p.x);
-            } else {
-                return Long.compare(this.y,p.y);
-            }
-        }
-        public String toString() {
-            return x + " " + y;
-        }
-        public boolean equals(Object o) {
-            pair x = (pair) o;
-            return (x.x == this.x && x.y == this.y);
-        }
-        }
-    static class FastReader 
-    { 
-        BufferedReader br; 
-        StringTokenizer st;
-        PrintWriter pw;
-  
-        public FastReader() 
-        { 
-            br = new BufferedReader(new
-                     InputStreamReader(System.in));
-            pw = new PrintWriter(new OutputStreamWriter(System.out));
-        }
-        public FastReader(boolean b)
-        {
-            try
-            {
-                br = new BufferedReader( new FileReader("input.txt")); 
-                pw = new PrintWriter("output.txt");
-            
-            }
-            catch(Exception e)
-            {
-
-            }
-
-        }
-        String next() 
-        { 
-            while (st == null || !st.hasMoreElements()) 
-            { 
-                try
-                { 
-                    st = new StringTokenizer(br.readLine()); 
-                } 
-                catch (IOException  e) 
-                { 
-                    e.printStackTrace(); 
-                } 
-            } 
-            return st.nextToken();
-        } 
-        int[] nextArray(int n)
-        {
-            int[] a=new int[n];
-			for (int i=0; i<n; i++) a[i]=nextInt();
-			return a;
-        }
-        int[][] nextArray2D(int m, int n)
-        {
-            int[][] a=new int[m][n];
-			for (int i=0; i<m; i++) 
-            {
-                for(int j=0;j<n;j++) 
-                    a[i][j]=nextInt();
-            }
-			return a;
-        }
-        int nextInt() 
-        { 
-            return Integer.parseInt(next()); 
-        } 
-  
-        long nextLong() 
-        { 
-            return Long.parseLong(next()); 
-        } 
-  
-        double nextDouble() 
-        { 
-            return Double.parseDouble(next()); 
-        } 
-  
-        String nextLine() 
-        { 
-            String str = ""; 
-            try
-            { 
-                str = br.readLine(); 
-            } 
-            catch (IOException e) 
-            { 
-                e.printStackTrace(); 
-            } 
-            return str; 
-        }
-        void print(Object...objects) {
-            for (int i = 0; i < objects.length; i++) {
-                if (i != 0)
-                    pw.print(' ');
-            pw.print(objects[i]);
-            }
-        }
-        void println(Object...objects)
-        {
-            for (int i = 0; i < objects.length; i++) {
-                if (i != 0)
-                    pw.print(' ');
-            pw.print(objects[i]);
-            }
-            pw.println();
-        }
-        void viewArray1D(int a[])
-        {
-            println(Arrays.toString(a));
-        }
-        void viewArray2D(int arr[][])
-        {
-            for (int[] row: arr)
-            viewArray1D(row);
-        }
-        void closer()
-        {
-            try{
-            br.close();
-            pw.flush();
-            pw.close();
-            }
-            catch(Exception e)
-            {
-            }
-        }
-    }
+ 
+		for(int i=0,j=0; i<s.length() && j<t.length();) {
+			if(s.charAt(i) == t.charAt(j)) {
+				System.out.print(s.charAt(i));
+				i++;
+				j++; 
+				continue;
+			}
+			if(dp[i][j] == dp[i+1][j]) i++;
+			else j++;
+		}
+		System.out.println();
+	}
 }
