69df5e4dda00da0682dc80813173c348fbd7430a
RohanTrix <52482556+RohanTrix@users.noreply.github.com>
2021-02-17 12:17:21 +0000

    Changes

--- Atcoder DP/F.java
+++ Atcoder DP/F.java
@@ -1,14 +1,31 @@
 import java.io.*;
 import java.util.*;
-public class E
+public class F
 {
+    //Refer AlgoExpert
     static void solve(FastReader sc)
     {
-        
+       
+        String first = sc.next();
+		String second = sc.next();
+
+		int[][] dp = new int[first.length()+1][second.length()+1];
+		for(int i = 1; i <= first.length(); i ++) 
+        {
+			for(int j = 1; j <= second.length(); j ++)
+             {
+				if(first.charAt(i-1) == second.charAt(j-1))
+                {
+					dp[i][j] = dp[i-1][j-1] + 1;
+				}
+				else {
+					dp[i][j] = Math.max(dp[i-1][j], dp[i][j-1]);
+				}
+			}
+		}
 
 
            
-    }
     
     
     public static void main(String[] args) 
