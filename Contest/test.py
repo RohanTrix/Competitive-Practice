a60484f48e8d806f7a2884a81d8e282a1cdc0a5f
RohanTrix <rohanyeahyeah@gmail.com>
2021-04-17 12:40:39 +0530

    Minor Change

--- Contest/test.py
+++ Contest/test.py
@@ -2,11 +2,4 @@ import sys
 sys.stdin = open('input.txt', 'r')  
 sys.stdout = open('output.txt', 'w')
 for t in range(int(input())):
-    n, k = map(int,input().split())
-    s = input()
-    c =0
-    for i in range(1,n//2+1):
-        if s[i-1]!=s[n-i]:
-            c+=1
-    ans = abs(k-c)
-    print(f"Case #{t+1}: {ans}")
+    pass
