76e1cb918dbf2b90b14f0b2051cfa414fb60d3ac
RohanTrix <rohanyeahyeah@gmail.com>
2021-04-29 08:35:29 +0530

    Changes

--- Contest/javarun.py
+++ Contest/javarun.py
@@ -1,8 +1,8 @@
 import os
 import time
-prog_name = "Solution.java"
-os.system(f'javac -d Classes {prog_name}')
+prog_name = "Main"
+os.system(f'javac -d Classes {prog_name}.java')
 t1 = time.time()
-os.system(f'java -cp Classes Solution local')
+os.system(f'java -cp Classes {prog_name} local')
 t2 = time.time()
 print("TIME TAKEN: ",t2-t1)
