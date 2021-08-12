6659ab980de971c6706cb10aac85cfef9c496e31
Rohan Sirohia <52482556+RohanTrix@users.noreply.github.com>
2021-08-12 23:23:28 +0530

    Minor Changes

--- Leetcode/StringAlgo/Design_Add_and_Search_Words_Data_Structure.java
+++ Leetcode/StringAlgo/Design_Add_and_Search_Words_Data_Structure.java
@@ -1,5 +1,10 @@
 package Leetcode.StringAlgo;
 
+/* Idea: Implementation of Trie with a DFS implementation of the search function 
+
+*/
+
+
 class Design_Add_and_Search_Words_Data_Structure {
 
     
@@ -37,21 +42,23 @@ class Design_Add_and_Search_Words_Data_Structure {
         public boolean dfs(TrieNode root, String s, int ind)
         {
             
-            if(ind==s.length())
+            if(ind==s.length()) // If reached end of string, return whether a word in Trie ends here or not
             {
                 return root.wordEnd;
             }
-            //System.out.println(s.charAt(ind));
-            if(s.charAt(ind) == '.')
+            
+            if(s.charAt(ind) == '.') // If '.' character is encounter
             {
                 boolean ans = false;
-                for(int i=0; i<128; i++)
+                for(int i=0; i<128; i++) // we will traverse each letter and send a dfs down its path if it 
+                                         // is not null. Even if one of the paths return true, our ans is true.
                 {
                     if(root.children[i]!=null)
                         ans = ans || dfs(root.children[i], s, ind+1);
                 }
                 return ans;
             }
+            // Else if valid character encountered, send dfs along this letter's path iff it exists(not null node)
             return (root.children[s.charAt(ind)]!=null) && dfs(root.children[s.charAt(ind)], s, ind +1);
         }
     }
