d6764cb91728f29106a2167a8d45d7b85c3edc15
Rohan Sirohia <52482556+RohanTrix@users.noreply.github.com>
2021-09-06 01:29:05 +0530

    Minor Changes

--- Question-Maker/temp.py
+++
@@ -1,8 +0,0 @@
-import os
-for i in range(1,10):
-    os.system(f"touch TT/input0{i}.txt")
-    os.system(f"touch TT/output0{i}.txt")
-
-os.system(f"touch TT/input10.txt")
-os.system(f"touch TT/output10.txt")
-
