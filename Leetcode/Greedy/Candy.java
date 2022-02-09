c6c99a1652caeced22457bb07a143b2614ec2cb6
Rohan Sirohia <52482556+RohanTrix@users.noreply.github.com>
2022-02-10 01:40:16 +0530

    Changes

--- Leetcode/Greedy/Candy.java
+++ Leetcode/Greedy/Candy.java
@@ -1,6 +1,20 @@
 /*
-    IDEA : The wrong though process wud be to go at every middle i (i haveing both neighbour) and then assigning it
-            the 
+    IDEA : The wrong though process wud be to go at every middle ith element (i having both neighbour) and 
+            then trying to assign coins to it....Whenever we are concerned with checking both neighbours
+            and assigning a value to current, we should think of doing a left pass and a right pass.
+
+            Here we do a left pass...and for every i that has higher rating than its neighbour i-1,
+            we will set ratings[i] = 1 + ratings[i-1]. 
+            This will ensure that if we have a larger ratings[i] than ratings[i-1], we have assigned i a
+            no. of coins just more than its prev neighbour.
+
+            Now we do a right to left pass, checking if i's rating is more than i+1...but here,
+            we need to remember that we have already assigned mini no. of coins based on one neighbour side.
+            So if the right neighbour has smaller rating, but has a smaller no. of coins than the left neighbour,
+            then we need to to keep left neighbours value only to satisfy both neighbours. This means taking
+            max of coins we would have to place for the 2 neighbours.
+
+            Finally, we just sum up the total number of coins used.
 */
 import java.util.stream.*;
 public class Candy
