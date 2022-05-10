e70e7f673e947e7701dd00fb4fd67ede93b196bb
Rohan Sirohia <52482556+RohanTrix@users.noreply.github.com>
2022-05-10 09:14:16 +0530

    Changes

--- Leetcode/Miscellaneous/Combination_Sum_III.java
+++ Leetcode/Miscellaneous/Combination_Sum_III.java
@@ -1,32 +1,22 @@
 import java.util.*;
 class Combination_Sum_III {
     
-    List<List<Integer>> res = new ArrayList();
-    
-    public void backtrack(int k, int sum, ArrayList<Integer> curr, int ind)
+    List<List<Integer>> res = new ArrayList<>();
+    public void recur(int ind, int k, int n, List<Integer> curr)
     {
-        if(sum==0 && k==0)
+        if(n == 0 && k==0)
         {
-            res.add(new ArrayList(curr));
+            res.add(new ArrayList<>(curr));
             return;
         }
-        if(k==0) return;
-        
-        for(int i = ind; i<10; i++)
-        {
-            if(i<=sum)
-            {
-                curr.add(i);
-                backtrack(k-1, sum-i,curr, i+1);
-                curr.remove(curr.size()-1);
-            }
-            else break;
-        }
-        
+        if(n<0 || ind==10) return;
+        recur(ind+1, k, n, curr);
+        curr.add(ind);
+        recur(ind+1, k-1, n-ind, curr);
+        curr.remove(curr.size()-1);
     }
     public List<List<Integer>> combinationSum3(int k, int n) {
-        backtrack(k,n,new ArrayList<Integer>(), 1);
+        recur(1, k, n, new ArrayList<>());
         return res;
-        
     }
 }
