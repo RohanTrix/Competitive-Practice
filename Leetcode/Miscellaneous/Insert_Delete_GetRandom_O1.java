22e6e0e4eb919d0daf1d89525961d649cd711c69
Rohan Sirohia <52482556+RohanTrix@users.noreply.github.com>
2022-06-07 00:39:31 +0530

    Imporved Insert Delete getrandom answer

--- Leetcode/Miscellaneous/Insert_Delete_GetRandom_O1.java
+++ Leetcode/Miscellaneous/Insert_Delete_GetRandom_O1.java
@@ -4,20 +4,22 @@ package Leetcode.Miscellaneous;
     IDEA : We use a HashMap and a ArrayList. The hashmap maps the value to the index it is stored in the arraylist
 
             1) For insertion, we insert the element to the list's end and 
-                add the (value-> index) in the map
+                add the (value-> last index) in the map
             
-            2) For removal, we decrease the index of all the elements after the one the to be removed,
-               should be decreased by one. then we remove the element from the list and the map.
+            2) For removal, we can do this in O(1) by removing the last element and ovewriting it on the position
+               of the element to be removed. We also update the new idx of this lastElement in the map. And of course remove the
+               value queried from the map.
 
             3) For random value, We choose a random index of the list and return the value present there.
 */
 public class Insert_Delete_GetRandom_O1 {
     HashMap<Integer, Integer> map;
     List<Integer> list;
-    
+    Random r;
     public RandomizedSet() {
         map = new HashMap<>();
         list = new ArrayList<>();
+        r = new Random();
     }
     
     public boolean insert(int val) {
@@ -28,22 +30,23 @@ public class Insert_Delete_GetRandom_O1 {
     }
     
     public boolean remove(int val) {
+
         if(!map.containsKey(val)) return false;
-        int index = map.get(val);
-        for(int i =index+1; i<list.size(); i++)
-        {
-            int v = list.get(i);
-            map.put(v, map.get(v)-1);
-        }
-        list.remove(index);
+        int idx = map.get(val);
         map.remove(val);
-        return true;
+        int lastElement = list.remove(list.size()-1);
+        if(idx == list.size())
+            return true;
+        else
+        {
+            map.put(lastElement, idx);
+            list.set(idx, lastElement);
+            return true;
+        }   
     }
-    
     public int getRandom() {
-        int min = 0, max = list.size()-1;
-        int random_int = (int)Math.floor(Math.random()*(max-min+1)+min);
-        return list.get(random_int);
+        int idx = r.nextInt(list.size());
+        return list.get(idx);
         
     }
 }
