d1b1cb29e852992b068b45f0491599de4252d88d
Rohan Sirohia <52482556+RohanTrix@users.noreply.github.com>
2023-01-26 12:33:50 +0530

    Changes

--- Leetcode/Graphs/Cheapest_Flights_Within_K_Stops.java
+++ Leetcode/Graphs/Cheapest_Flights_Within_K_Stops.java
@@ -4,61 +4,38 @@
  *             This is because there can be different paths to the same node and one path may not be best distance wise but may be better
  *             in a way that it leaves more stops.
  */
-
+import java.util.*;
 public class Cheapest_Flights_Within_K_Stops {
-    Map<Integer, List<Node>> edges = new HashMap<>();
-    Set<Node> visited = new HashSet<>();
     public int findCheapestPrice(int n, int[][] flights, int src, int dst, int k) {
-        PriorityQueue<Node> pq = new PriorityQueue<>((a,b) -> a.cost - b.cost);
-        
-        for(int i = 0; i<n; i++) edges.put(i, new ArrayList<>());
-        for(int flight[] : flights)
-        {
-            edges.get(flight[0]).add(new Node(flight[1],-1, flight[2]));
+        Map<Integer, List<Node>> map = new HashMap<>();
+        for(int f[] : flights){
+            map.computeIfAbsent(f[0], key -> new ArrayList<>()).add(new Node(f[1], f[2], -1));
         }
-        
-        int dist[][] = new int[101][k+5];
-        for(int d[] : dist) Arrays.fill(d, Integer.MAX_VALUE);
-        dist[src][k+1] = 0;
-        pq.offer(new Node(src, k+1, 0));
-        while(!pq.isEmpty())
-        {
-            // System.out.println(pq);
+        k++;
+        PriorityQueue<Node> pq = new PriorityQueue<>((a,b) -> a.cost() - b.cost());
+        pq.offer(new Node(src, 0, k));
+        Set<Node> visited = new HashSet<>();
+        visited.add(new Node(src,0, k));
+        int dist[][] = new int[101][k+1];
+        for(int a[] : dist) Arrays.fill(a, Integer.MAX_VALUE);
+        dist[src][k] = 0;
+        while(!pq.isEmpty()){
             Node curr = pq.poll();
             visited.add(curr);
-            if( curr.node == dst) return dist[dst][curr.k];
-            if( dist[curr.node][curr.k] < curr.cost || curr.k == 0)
-                continue;
-            for(Node to : edges.get(curr.node))
-            {
-                if(visited.contains(to.node)) continue;
+            if(curr.node == dst)
+                return dist[curr.node][curr.k];
+            if( dist[curr.node][curr.k] < curr.cost || curr.k == 0 ) continue;
+
+            for(Node to : map.getOrDefault(curr.node, new ArrayList<>())){
+                if(visited.contains(to)) continue;
                 int newDist = dist[curr.node][curr.k] + to.cost;
-                if(dist[to.node][curr.k-1] > newDist)
-                {
-                    dist[to.node][curr.k-1] = newDist;
-                    pq.offer(new Node(to.node, curr.k-1, newDist));
-                }
+                if(dist[to.node][curr.k-1] <= newDist) continue;
+                dist[to.node][curr.k-1] = newDist;
+                Node nextNode = new Node(to.node, newDist, curr.k-1);
+                pq.offer(nextNode);
             }
         }
         return -1;
     }
-    class Node
-    {
-        int node, k, cost;
-        public Node(int node, int k, int cost)
-        {
-            this.node = node;
-            this.k = k;
-            this.cost = cost;
-        }
-        public boolean equals(Object o)
-        {
-            Node p = (Node)o;
-            return  node == p.node && k == p.k && cost == p.cost;
-        }
-        public int hashCode()
-        {
-            return Objects.hash(node, k, cost);
-        }
-    }
+    public record Node(int node, int cost, int k) {}
 }
